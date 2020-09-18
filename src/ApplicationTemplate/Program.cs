ef170b403f1a5ff3148a7eac2bd0411d6bad907d
Shawn Shi <ss.shawnshi@gmail.com>
2020-09-17 21:40:20 -0400

    initial commit - new console application

---
+++ src/ApplicationTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApplicationTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
